143456489738bae2eefe13fc87dfe9842afa5e37
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-07-14 15:27:43 -0400

    aplicando seguridad

--- src/main/java/cl/scd/LoginController.java
+++ src/main/java/cl/scd/LoginController.java
@@ -1,5 +1,28 @@
 package cl.scd;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.scd.service.ILoginService;
+import cl.scd.service.IResetTokenService;
+import cl.scd.util.EmailUtil;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+	
+	@Autowired
+	private ILoginService service;
+	
+	@Autowired
+	private IResetTokenService tokenService;
+	
+	@Autowired
+	private EmailUtil emailUtil;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
 
 }
